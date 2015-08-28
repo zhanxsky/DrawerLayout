967c9d2bbd0cdfd0be8adddcf2abbcd0a600c0dc
zhanxsky <zhan1009@hotmail.com>
2015-08-28 14:45:45 +0800

    aaa

--- app/src/main/java/com/zhan/drawerlayout/MainActivity.java
+++ app/src/main/java/com/zhan/drawerlayout/MainActivity.java
@@ -1,37 +1,91 @@
 package com.zhan.drawerlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayList<String> menuLists;
+    private ArrayAdapter<String> adapter;
+//    private ActionBarDrawerToggle mDrawerToggle;
+    private String mTittle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        menuLists = new ArrayList<String>();
+        mTittle = (String) getTitle();
+//        mDrawerToggle = new ActionBarDrawerToggle(
+//                this,
+//                mDrawerLayout,
+//                R.mipmap.ic_drawer,
+//                R.string.drawer_open,
+//                R.string.drawer_close){
+//            @Override
+//            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+//                getActionBar().setTitle("请选择");
+//                invalidateOptionsMenu();  //call onprepareoptionsMenu
+//
+//            }
+//
+//            @Override
+//            public void onDrawerClosed(View drawerView) {
+//                super.onDrawerClosed(drawerView);
+//                getActionBar().setTitle(mTittle);
+//                invalidateOptionsMenu();
+//            }
+//        };
+//        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+        for (int i = 0; i < 5; i++) {
+            menuLists.add("zhan" + i);
+            adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,menuLists);
+            mDrawerList.setAdapter(adapter);
+        }
+        mDrawerList.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //插入一个Fragment到FrameLayout中
+        Fragment contentFragment = new ContentFragment();
+        Bundle args = new Bundle();
+        args.putString("text", menuLists.get(position));
+        contentFragment.setArguments(args);
+
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.content_frame,contentFragment).commit();
+
+
+        mDrawerLayout.closeDrawer(mDrawerList);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
